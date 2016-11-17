c4f489010b198ff31aa0024c0035a2985776eb19
Kirk Abbott <kirkabbott1@gmail.com>
2016-11-17 15:06:59 -0500

    initial commit

---
+++ directives.js
@@ -0,0 +1,16 @@
+// var app = angular.module('app', ['ui.router']);
+//
+// app.directive('hello', function() {
+//   return {
+//     scope: {
+//       'person': '@'
+//     },
+//     controller: function($scope) {
+//       if (!$scope.person) {
+//         $scope.person = 'World'
+//         template2 = '<h1>Hello, {{person}}</h1>'
+//       }
+//     },
+//     template: '<h1>Hello, {{person}}</h1>'
+//   }
+// });
